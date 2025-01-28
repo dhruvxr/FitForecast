b3134a534c0b7eba10e788486f1eb9e66896d96d
Dhruv Pabbi <dhruvpabbi@gmail.com>
2025-01-28 15:36:04 -0600

    commit

---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+import StudentInfo from './student-info.js';
+import React from 'react';
+  
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome to Week 2</h1>  
+      <StudentInfo />  
+    </div>
+  );
+}
+
+export default Page;

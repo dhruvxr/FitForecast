f851168a8b97b0f842ff7d4b228b1ca00a51c0db
Dhruv Pabbi <dhruvpabbi@gmail.com>
2025-02-11 09:35:49 -0600

    Added week-4 page with quantity selector component

--- app/page.js
+++ app/page.js
@@ -9,7 +9,10 @@ const Page = () => {
          <a href="/week-2" style={{ color: 'blue' }}>Week 2 Page</a> 
       </p>
       <p>
-        Visit the <a href="/week-3" style={{ color: 'blue' }}>Week 3 Page</a> for the latest assignments.
+         <a href="/week-3" style={{ color: 'blue' }}>Week 3 Page</a> 
+      </p>
+      <p>
+        Visit the <a href="/week-4" style={{ color: 'blue' }}>Week 4 Page</a> for the latest assignments.
       </p>
     </div>
   );
---
+++ app/week-4/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <NewItem />
+    </div>
+  );
+}
+
+export default Page;

28d2d84ec8b96332ed28ea56dff7edf2e3af9a31
Dhruv Pabbi <161884115+dhruvxr@users.noreply.github.com>
2025-03-18 01:14:38 -0500

    Update item.js

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -4,7 +4,7 @@ const Item = ({ name, quantity, category, onSelect }) => {
   return (
     <li 
       className="p-4 bg-white shadow-md rounded-lg flex justify-between items-center border border-gray-200 cursor-pointer hover:bg-gray-100"
-      onClick={() => onSelect(name)} // Click to select
+      onClick={() => onSelect(name)} 
     >
       <span className="font-semibold text-black">{name}</span> 
       <span className="text-black">Qty: {quantity}</span> 
